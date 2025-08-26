028bad0489742f097804d169a238a3639decc2ba
Iurii Volobuev <iurii.volobuev@gmail.com>
2025-08-26 15:59:03 +0200

    Create credentials without specifying profile

--- src/main/java/app/DynamoDb.java
+++ src/main/java/app/DynamoDb.java
@@ -1,6 +1,7 @@
 package app;
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
@@ -10,9 +11,8 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import java.net.URI;
 
 class DynamoDb {
-    private static final String PROFILE_LOCAL = "local";
     private static final DynamoDbClient CLIENT = DynamoDbClient.builder().region(Region.EU_CENTRAL_1)
-            .credentialsProvider(ProfileCredentialsProvider.create(PROFILE_LOCAL))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy", "dummy")))
             .endpointOverride(URI.create("http://localhost:8000")).build();
     private static final DynamoDbEnhancedClient ENHANCED_CLIENT = DynamoDbEnhancedClient.builder().dynamoDbClient(CLIENT).build();
     private static final DynamoDbTable<Dog> DOG_TABLE = ENHANCED_CLIENT.table("dog", TableSchema.fromBean(Dog.class));
